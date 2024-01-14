74b6cfde3ee7919393b0835747500061371fe9d1
Nilton Amaral <nilton.a.junior@hotmail.com>
2024-01-14 17:00:50 -0300

    feat: docker compose finish

--- challenge-delliv-backend/prisma/seed/init.ts
+++ challenge-delliv-backend/prisma/seed/init.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import * as bcrypt from 'bcrypt';
+import { Status } from "@prisma/client";
 const prisma = new PrismaClient()
 async function main() {
   const admin = await prisma.user.upsert({
@@ -11,7 +12,71 @@ async function main() {
       password: await bcrypt.hash('admin', 10)
     },
   })
-  console.log({ admin })
+  const create = await prisma.order.createMany({
+    data: [
+        {
+            client_name: 'Usuario 1',
+            street: 'Rua usuario 1',
+            zipCode: "84070020",
+            number: '957',
+            state: 'PR',
+            city: 'Ponta Grossa',
+            neighborhood: 'Nova Russia',
+            status: Status.PENDING
+        },
+        {
+            client_name: 'Usuario 1',
+            street: 'Rua usuario 1',
+            zipCode: "84070020",
+            number: '957',
+            state: 'PR',
+            city: 'Ponta Grossa',
+            neighborhood: 'Nova Russia',
+            status: Status.PENDING
+        },
+        {
+            client_name: 'Usuario 2',
+            street: 'Rua usuario 2',
+            zipCode: "84070020",
+            number: '957',
+            state: 'PR',
+            city: 'Ponta Grossa',
+            neighborhood: 'Nova Russia',
+            status: Status.PENDING
+        },
+        {
+            client_name: 'Usuario 3',
+            street: 'Rua usuario 3',
+            zipCode: "84070020",
+            number: '957',
+            state: 'PR',
+            city: 'Ponta Grossa',
+            neighborhood: 'Nova Russia',
+            status: Status.PENDING
+        },
+        {
+            client_name: 'Usuario 4',
+            street: 'Rua usuario 4',
+            zipCode: "84070020",
+            number: '957',
+            state: 'PR',
+            city: 'Ponta Grossa',
+            neighborhood: 'Nova Russia',
+            status: Status.PENDING
+        },
+        {
+            client_name: 'Usuario 5',
+            street: 'Rua usuario 5',
+            zipCode: "84070020",
+            number: '957',
+            state: 'PR',
+            city: 'Ponta Grossa',
+            neighborhood: 'Nova Russia',
+            status: Status.PENDING
+        },
+    ]
+  })
+  console.log({ admin, create })
 }
 main()
   .then(async () => {
